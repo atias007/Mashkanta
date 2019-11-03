d4498f82aaf98d08f6afc611072201b610090bc7
tsahi <tsahiatias@gmail.com>
2019-11-03 11:52:38 +0200

    Git Commit

--- Mashkanta/Entities/CourseResult.cs
+++ Mashkanta/Entities/CourseResult.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Mashkanta
 {
@@ -22,5 +23,17 @@ namespace Mashkanta
         public double RemainingFund { get; set; }
 
         public List<Payment> Payments { get; private set; }
+
+        public void SetVariables()
+        {
+            // for Git Commit
+            TotalReturn = Utils.Round2(Payments.Sum(p => p.TotalPayment));
+            TotalInterestAndPriceIndex = Utils.Round2(Payments.Sum(p => p.InterestPayment) + Payments.Sum(p => p.FundPaymentWithPriceIndex) - Payments.Sum(p => p.FundPayment));
+            MaxMonthReturn = Payments.Max(p => p.TotalPayment);
+            MinMonthReturn = Payments.Min(p => p.TotalPayment);
+            Ratio = Utils.Round2(TotalReturn / Payments.Sum(p => p.FundPayment));
+            var payment = Payments.Last();
+            RemainingFund = Utils.Round2(payment.TotalFund - payment.FundPayment);
+        }
     }
 }
